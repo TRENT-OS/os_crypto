16135c5a12a86117e02a389eec92e07e9073b4c5
Carmelo Pintaudi <c.pintaudi@gmail.com>
2019-07-25 16:51:48 +0200

    [NEW] SEOS-379: add include/seos/SeosCryptoCipher_Impl.h

---
+++ include/seos/SeosCryptoCipher_Impl.h
@@ -0,0 +1,33 @@
+/* Copyright (C) 2019, Hensoldt Cyber GmbH
+ *
+ * @addtogroup SEOS
+ * @{
+ *
+ * @file SeosCryptoCipher_Impl.h
+ *
+ * @brief Underlying implementation related definitions of SeosCryptoCipher
+ *
+ */
+
+#pragma once
+
+#include "mbedtls/rsa.h"
+#include "mbedtls/aes.h"
+
+struct SeosCryptoCipher
+{
+    union
+    {
+        mbedtls_aes_context     aes;
+        mbedtls_rsa_context     rsa;
+    }
+    agorithmCtx;
+
+    SeosCryptoCipher_Algorithm  algorithm;
+    SeosCryptoKey const*        key;
+    SeosCryptoRng*              rng;
+    void*                       iv;
+    size_t                      ivLen;
+    char                        outputBuf[SeosCryptoCipher_OUTPUT_BUFFER_SIZE];
+    char                        tag[SeosCryptoCipher_TAG_BUFFER_SIZE];
+};
