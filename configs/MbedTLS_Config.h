4eda0006d57846c590994848180855682b6e0d8e
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2019-11-14 16:58:27 +0100

    [FIX] Clean up mbedTLS dependencies

--- configs/MbedTLS_Config.h
+++ configs/MbedTLS_Config.h
@@ -1,27 +1,34 @@
 #ifndef MBEDTLS_CONFIG_H
 #define MBEDTLS_CONFIG_H
 
+/*
+ * For now, the config for seos_crypto_api and seos_tls_api need to be identical
+ * so that all the data structures are similar!!!
+ */
+
+#define MBEDTLS_DEBUG_C
+
+// -----------------------------------------------------------------------------
+// Modules for seos_crypto_api
+// -----------------------------------------------------------------------------
+
+#define MBEDTLS_MD_C
+#define MBEDTLS_MD5_C
 #define MBEDTLS_SHA256_C
-#define MBEDTLS_SHA512_C
-#define MBEDTLS_ENTROPY_C
-#define MBEDTLS_NO_PLATFORM_ENTROPY
 
 #define MBEDTLS_CIPHER_C
 #define MBEDTLS_AES_C
 #define MBEDTLS_GCM_C
 #define MBEDTLS_CIPHER_MODE_CBC
+
 #define MBEDTLS_CTR_DRBG_C
 
-#define MBEDTLS_MD_C
-#define MBEDTLS_MD5_C
-
 #define MBEDTLS_OID_C
-#define MBEDTLS_BIGNUM_C
 #define MBEDTLS_PKCS1_V15
 #define MBEDTLS_PKCS1_V21
-#define MBEDTLS_SHA1_C
 #define MBEDTLS_RSA_C
 
+#define MBEDTLS_BIGNUM_C
 #define MBEDTLS_GENPRIME
 
 #define MBEDTLS_ECP_C
@@ -29,12 +36,23 @@
 #define MBEDTLS_ECDH_C
 
 #define MBEDTLS_DHM_C
-/*
- * You should adjust this to the exact number of sources you're using: default
- * is the "platform_entropy_poll" source, but you may want to add other ones
- * Minimum is 2 for the entropy test suite.
- */
-#define MBEDTLS_ENTROPY_MAX_SOURCES 2
+
+// -----------------------------------------------------------------------------
+// Modules for seos_tls_api
+// -----------------------------------------------------------------------------
+
+#define MBEDTLS_SSL_PROTO_TLS1_2
+#define MBEDTLS_SSL_CLI_C
+#define MBEDTLS_SSL_TLS_C
+
+#define MBEDTLS_KEY_EXCHANGE_DHE_RSA_ENABLED
+
+#define MBEDTLS_X509_CRT_PARSE_C
+#define MBEDTLS_X509_USE_C
+#define MBEDTLS_PK_PARSE_C
+#define MBEDTLS_PK_C
+#define MBEDTLS_ASN1_PARSE_C
+
 #include "mbedtls/check_config.h"
 
 #endif /* MBEDTLS_CONFIG_H */
