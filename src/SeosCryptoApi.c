1ce72d9b1eb90c713ddc68920cb761b676666d8e
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2020-03-13 18:36:33 +0100

    [REM] Clean up old Macros

--- src/SeosCryptoApi.c
+++ src/SeosCryptoApi.c
@@ -15,47 +15,38 @@
 
 // -------------------------- defines/types/variables --------------------------
 
-// A proxy object could be NULL, if so, just pass NULL
-#define GET_OBJ(p, o) ((p) == NULL ? NULL : (p)->o)
+// Call function from self pointer
+#define CALL(s, f, ...)                                             \
+    (NULL == (s)) ? SEOS_ERROR_INVALID_PARAMETER :                  \
+        (NULL == (s)->impl.vtable->f) ? SEOS_ERROR_NOT_SUPPORTED :  \
+        (s)->impl.vtable->f((s)->impl.context, __VA_ARGS__)         \
 
-// We call a function from a proxy object's API. Make sure that the API call
-// is actually implemented.
-#define CALL_IMPL(p, f, ...)                                        \
-    (NULL == p) ? SEOS_ERROR_INVALID_PARAMETER :                    \
-        (NULL == p->impl.vtable->f) ? SEOS_ERROR_NOT_SUPPORTED :    \
-        p->impl.vtable->f(p->impl.context, __VA_ARGS__)             \
-
-// Initialize a proxy object from existing API pointer
-#define INIT_PROXY(p, c) {                      \
-    if (NULL == p || NULL == c) {               \
-        return SEOS_ERROR_INVALID_PARAMETER;    \
-    }                                           \
-    memset(p, 0, sizeof(*p));                   \
-    p->impl = c->impl;                          \
-}
-
-// Allocate proxy object and set its API handle
-#define PROXY_INIT(p, c)                                                \
-    if (NULL == &(p) || NULL == (c)) {                                  \
+// Allocate proxy object and set its API handle to self pointer
+#define PROXY_INIT(p, s)                                                \
+    if (NULL == &(p) || NULL == (s)) {                                  \
         return SEOS_ERROR_INVALID_PARAMETER;                            \
     }                                                                   \
-    if(((p) = c->memIf.malloc(sizeof(SeosCryptoApi_Proxy))) == NULL) {  \
+    if(((p) = s->memIf.malloc(sizeof(SeosCryptoApi_Proxy))) == NULL) {  \
         return SEOS_ERROR_INSUFFICIENT_SPACE;                           \
     }                                                                   \
-    (p)->hCrypto = (c);
+    (p)->hCrypto = (s);
+
 // Free proxy object with associated API context's mem IF
 #define PROXY_FREE(p)                           \
     if (NULL == (p)) {                          \
         return SEOS_ERROR_INVALID_PARAMETER;    \
     }                                           \
     (p)->hCrypto->memIf.free(p);
+
 // Call function from proxy objects API handle
 #define PROXY_CALL(p, f, ...)                                                   \
     (NULL == (p)) ? SEOS_ERROR_INVALID_PARAMETER :                              \
         (NULL == (p)->hCrypto->impl.vtable->f) ? SEOS_ERROR_NOT_SUPPORTED :     \
         (p)->hCrypto->impl.vtable->f((p)->hCrypto->impl.context, __VA_ARGS__)   \
+
 // Get object from proxy
 #define PROXY_GET_OBJ(p) ((NULL == (p)) ? NULL : (p)->obj)
+
 // Get object specific pointers to object from proxy
 #define PROXY_GET_OBJ_PTR(p) ((NULL == (p)) ? NULL : &(p)->obj)
 #define PROXY_GET_AGREE_PTR(p) \
@@ -248,7 +239,7 @@ SeosCryptoApi_Rng_getBytes(
     void*                        buf,
     const size_t                 bufSize)
 {
-    return CALL_IMPL(self, Rng_getBytes, flags, buf, bufSize);
+    return CALL(self, Rng_getBytes, flags, buf, bufSize);
 }
 
 seos_err_t
@@ -257,7 +248,7 @@ SeosCryptoApi_Rng_reseed(
     const void*    seed,
     const size_t   seedSize)
 {
-    return CALL_IMPL(self, Rng_reseed, seed, seedSize);
+    return CALL(self, Rng_reseed, seed, seedSize);
 }
 
 // ------------------------------- MAC API -------------------------------------
@@ -581,7 +572,7 @@ SeosCryptoApi_Key_loadParams(
     void*                         keyParams,
     size_t*                       paramSize)
 {
-    return CALL_IMPL(self, Key_loadParams, name, keyParams, paramSize);
+    return CALL(self, Key_loadParams, name, keyParams, paramSize);
 }
 
 // ------------------------------ Cipher API -----------------------------------
